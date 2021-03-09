7f855c860d3a7364edd50ef10fa13ff1f7ca1a10
StephenHe <852630479@qq.com>
2021-03-09 17:32:48 +0800

    first commit

---
+++ src/test/java/cn/edu/ncu/stephenhe/gitstudy/GitstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.ncu.stephenhe.gitstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
